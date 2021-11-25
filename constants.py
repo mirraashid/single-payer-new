c3194492e98b33b390dbc84f6d7104ca19329bf4
Mir <mirrashidmir009@gmail.com>
2021-11-26 01:02:24 +0530

    first commit

---
+++ constants.py
@@ -0,0 +1,34 @@
+#Graph API keys
+GRAP_API = {
+    'clientId': 'a47c5bb1-d6c5-4b15-ad04-4965a6a52200',
+    'clientSecret': 'Yid05ISF3R3vI1loFlLzTE6RLZTHUTT40cSpJcgLkso=',
+    'tenantId': 'singlepayercost.onmicrosoft.com',
+    'verion' : 'v1.0',
+    'userId' : 'a01ab9eb-36de-4ebc-9889-3eb61d16d0e8',
+    'driveId' : 'b!8BpoJu__qEGSPkgzdPS-tpKUfASPeq9KuGIiQ7kUjgFvDlgFQHDHTYFuK27A0xlp',
+    'fileId' : '01X34IAM6VN65V2T75A5HYERTRRMYYHROJ',
+    'newFileId' : '01X34IAM6VN65V2T75A5HYERTRRMYYHROJ',
+    'baseUrl' : 'https://graph.microsoft.com/v1.0/users/a01ab9eb-36de-4ebc-9889-3eb61d16d0e8/drives/b!8BpoJu__qEGSPkgzdPS-tpKUfASPeq9KuGIiQ7kUjgFvDlgFQHDHTYFuK27A0xlp/items/01X34IAM6VN65V2T75A5HYERTRRMYYHROJ/'
+}
+
+#Firebase
+FIREBASE_CREDS = {
+		"apiKey": "AIzaSyCpsDdfpHlfcjJAZgGtmdBM7K3zKWaMCNE",
+		"authDomain": "single-payer.firebaseapp.com",
+		"databaseURL": "https://single-payer-default-rtdb.firebaseio.com/",
+		"storageBucket": "single-payer.appspot.com",
+		"serviceAccount": {
+            "type": "service_account",
+            "project_id": "single-payer",
+            "private_key_id": "d46fe15320733cdb208efcc541c68fbddb5edd2d",
+            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDG3gr7t/NsU5JS\nqJi6f0Koonb57n24epAvXZrAx/Qjpvi0ut7G/cg6//1QSilwtu8yy1yiqSP39nvs\n4WotXLraCAI139WxUcAKmfA7Xr540voGHzeykJ/P365sVW/8B35AEcJUN0eRR2iy\nwgtyEyHfB88l0wxpI8q/4Wph8Y2/QDYihJTboq5bBX4bgOfLG0Om4vxahzJH1ihI\ngZoE96vxnzCFoHxPuir3ilXlJcvy5w9XM4TlXlaNII6Ho870K3IqpYYm07af6Nxb\nnVvhyAP1kj+pcT1IGyO6oaT+EKiMl0a6694cal3FVPKvv/Y3Q1j7sRai9h4G5yjb\nXnGFgWENAgMBAAECggEAKg5ADV6n/HMqARAFiH8sNiRxe8Z7UN2ei13EwdsnXrjL\nZNEWFsgJpayIQggE/oC5UikelImngSNfrsc24JP5Me26pfEJrH+o2LUYUD3nkv6v\nAEduzHT4TwozRVbGIWrmQJ9g1NWB7Y/H7PHLYNr58UQiAxJAUCXRFUYS7NacDuMh\n/VE8gOZQfv2c2rZ+kVHUBptxGjuM8WDUeLUcEg65cpetaCyTnNdGUBRc5psP/pcZ\nGBgORuNuWqBwaqMKpd2mjw0WFHhOjPKEutkuYyISqQvRbAUoRsUa/xuPpFeWsSiw\nYiha9h+tRlsyTPU/Z5dzGSoSU3xhBCH2KOLNhY96owKBgQD6SJuQht1KPer/mLh7\nArwLuydDNvrFC3fa+lJt+KuwxHSd+MHBHMPVaMt9wMD1nDWHyYosiN3z+Sv8IWYy\nYd22JT95ch6v9es12eFmuVMNzGwkuM1A5MFK2HsVzC0BNwtLkQva3vvJ3lWvr1in\nfl3vUNbH4PB90SUzmxUHfj4VEwKBgQDLaM6+v4AW6HyRu7ato1uZ2LMOb2NTRqk0\nesWfhrMA5+W9bwMH5tquHrJW+XVisDia6rA4AqBo0fK5VcPmfpbot6ayLZWBNwjl\nYoKvBtl3cAqQwGbgyM46hIKpKsFTEuCrlW/fXDrcTvGUu7fJ3zeT77W3c3EzjboC\nklOoeO8VXwKBgE2AiluYx5Gj1s9LNWUyFZT0YVYZAlJeGMFAXqIbLN9SFmKMk0L/\na0z8ZHcOZzInKxRfPmBICaLw98Pde84CLCupIRy0T/hhCHLmgzvSC3vsX1iHuVds\n/sX0fzjivtTvfJnr0mX2QrXiGVqLy8IiCWh/+eJvL0lmqERws3RcjVDtAoGBAL/V\n6KYdFUCFonYPAwpftlQx+yzyAErZqVrFLvx0nP2Nty4hNEqPfk4YxTkhq4OXjssL\n64XndEFnP/Nq34bSfKCeE6iXNRjYPpuXfIh1fUMXd99slZoJx/WIY3IH0Kv4cctg\n9FFDOn/sNAqBv6Oz0UbEHC0XwvtL/tHODvgkldchAoGAHhhPGGrmF8hdToHu5RPz\nMBmctTsZHRXZ66z0obtzjNO9/BIfgn0wr5KJKsLF3dQKMNkBudvxuyL9FVnNpyNA\nG8hxl0+nq7nRbWQ+3IyNL2lmrlZF0X8hzJpuyHjl98l3HRLHkg+rIin+7Y21njxh\nHtI2BU3hydaLM8MvXou8+DU=\n-----END PRIVATE KEY-----\n",
+            "client_email": "firebase-adminsdk-zgnbz@single-payer.iam.gserviceaccount.com",
+            "client_id": "114971881975759451811",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zgnbz%40single-payer.iam.gserviceaccount.com"
+            }
+	}
+     
+
